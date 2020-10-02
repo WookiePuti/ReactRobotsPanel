45f0a452d0452e8c303290cbe79542f824b62652
Wookie9412 <lukasputon@gmail.com>
2020-10-02 17:43:46 +0200

    Updates

---
+++ precache-manifest.cd4df750365111c821b34ac06fc98729.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03c7287f0fcb5aa238c2926a7c54a5f2",
+    "url": "/ReactRobotsPanel/index.html"
+  },
+  {
+    "revision": "0adb063cae1b3b5caf68",
+    "url": "/ReactRobotsPanel/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "8b34645570576e089ec2",
+    "url": "/ReactRobotsPanel/static/css/main.9f6b8aee.chunk.css"
+  },
+  {
+    "revision": "0adb063cae1b3b5caf68",
+    "url": "/ReactRobotsPanel/static/js/2.270c9da8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactRobotsPanel/static/js/2.270c9da8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b34645570576e089ec2",
+    "url": "/ReactRobotsPanel/static/js/main.bc83403d.chunk.js"
+  },
+  {
+    "revision": "c84040f7ddfc7c0f5394",
+    "url": "/ReactRobotsPanel/static/js/runtime-main.57cdbf4c.js"
+  },
+  {
+    "revision": "a86dcee57798643825def135fcf6d17e",
+    "url": "/ReactRobotsPanel/static/media/Rosemary.a86dcee5.woff"
+  }
+]);
